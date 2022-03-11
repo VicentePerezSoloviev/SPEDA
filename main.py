56d80f3543a711c0c5b57c715a260c69e73ea475
Vicente Perez Soloviev <vicente.perez.soloviev@gmail.com>
2022-03-11 18:36:43 +0100

    update main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from SPEDA import SpEDA
-from benchmarks import rastrigins_function
+from benchmarks import Benchmarking
 
 num_vars = 3
 variables = [str(i) for i in range(num_vars)]
@@ -10,7 +10,10 @@ initial_vector = initial_vector.set_index('data')
 initial_vector.loc['mu'] = [0]*num_vars
 initial_vector.loc['std'] = 100
 
-eda = SpEDA(alpha=0.7, max_it=1000, dead_it=150, size_gen=200, cost_function=rastrigins_function, vector=initial_vector)
+benchmarking = Benchmarking(num_vars)
+
+eda = SpEDA(alpha=0.7, max_it=1000, dead_it=150, size_gen=200,
+            cost_function=benchmarking.rastrigins_function, vector=initial_vector)
 best_cost, best_ind, history = eda.run()
 print(best_ind, best_cost)
 print(history)
